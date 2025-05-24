7ab45741fc4178b5ac95a42071fab08dfd4501a9
Nicolo Dellepiane <nicolo.dellepiane@mail.polimi.com>
2025-05-24 19:32:50 +0200

    parallel training (added vectrorized environments)

--- policy_training/train_policy.py
+++ policy_training/train_policy.py
@@ -1,7 +1,8 @@
 from stable_baselines3 import PPO
 from SpotmicroEnv import SpotmicroEnv
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.env_checker import check_env
-from reward_function import reward_function
+from reward_function import reward_function, init_custom_state
 
 TOTAL_STEPS = 4_000_000
 
@@ -10,18 +11,39 @@ def clipped_linear_schedule(initial_value, min_value=1e-5):
         return max(progress_remaining * initial_value, min_value)
     return schedule
 
-env = SpotmicroEnv(use_gui=False, reward_fn=reward_function, dest_save_file="state4M-3.pkl")
-check_env(env, warn=True) #optional
+def make_env(rank):
+    def _init():
+        env = SpotmicroEnv(
+            use_gui=False,
+            reward_fn=reward_function,
+            init_custom_state=init_custom_state,
+            dest_save_file=f"state4M-{rank}.pkl"
+        )
+        env.seed(100 + rank)
+        return env
+    return _init
 
-model = PPO(
-    "MlpPolicy", 
-    env, 
-    verbose = 1, 
-    learning_rate=clipped_linear_schedule(3e-4),
-    ent_coef=0.002, #previously 0.0015
-    clip_range=0.1,
-    tensorboard_log="./logs"
-)
-model.learn(total_timesteps=TOTAL_STEPS)
-model.save("ppo_walk4M-3")
-env.close()
+def main():
+    num_envs = 12
+    vec_env = SubprocVecEnv([make_env(rank) for rank in range(num_envs)])
+
+    model = PPO(
+        "MlpPolicy", 
+        vec_env, 
+        verbose=0, 
+        learning_rate=clipped_linear_schedule(3e-4),
+        ent_coef=0.002,
+        clip_range=0.1,
+        tensorboard_log="./logs"
+    )
+
+    model.learn(total_timesteps=TOTAL_STEPS)
+    model.save("ppo_walk4M-3-norm")
+    vec_env.close()
+
+if __name__ == '__main__':
+    import multiprocessing
+    multiprocessing.set_start_method('spawn', force=True)  # safe in Docker
+    main()
+
+#400k at 11:55
